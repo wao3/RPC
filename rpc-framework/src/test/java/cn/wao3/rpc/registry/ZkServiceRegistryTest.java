ab8a01f902343d3008568294ec6aebfc8bbd8ba3
WangAo <wao1999@qq.com>
2021-05-16 17:53:47 +0800

    服务注册测试

---
+++ rpc-framework/src/test/java/cn/wao3/rpc/registry/ZkServiceRegistryTest.java
@@ -0,0 +1,35 @@
+package cn.wao3.rpc.registry;
+
+import cn.wao3.rpc.dto.RpcRequest;
+import cn.wao3.rpc.registry.zk.ZkServiceDiscovery;
+import cn.wao3.rpc.registry.zk.ZkServiceRegistry;
+import cn.wao3.rpc.registry.zk.ZkUtils;
+import org.junit.Test;
+
+import java.net.InetSocketAddress;
+import java.util.Arrays;
+import java.util.List;
+
+public class ZkServiceRegistryTest {
+
+    private final ServiceRegistry serviceRegistry = new ZkServiceRegistry();
+    private final ZkServiceDiscovery serviceDiscovery = new ZkServiceDiscovery();
+
+    @Test
+    public void registerAndDiscover() {
+        RpcRequest rpcRequest = new RpcRequest();
+        rpcRequest.setInterfaceName("HelloService");
+        rpcRequest.setGroup("Test");
+        rpcRequest.setVersion("1");
+        List<InetSocketAddress> inetSocketAddresses = Arrays.asList(new InetSocketAddress("127.0.0.1", 8888),
+                new InetSocketAddress("127.0.0.2", 8888),
+                new InetSocketAddress("127.0.0.3", 8888),
+                new InetSocketAddress("127.0.0.4", 8888));
+        inetSocketAddresses.forEach(inetSocketAddress -> serviceRegistry.register(rpcRequest.getRpcServiceName(), inetSocketAddress));
+        for (int i = 0; i < 10; i++) {
+            InetSocketAddress discover = serviceDiscovery.discover(rpcRequest);
+            System.out.println(discover);
+        }
+        inetSocketAddresses.forEach(ZkUtils::clearRegistry);
+    }
+}
